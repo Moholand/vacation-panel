91dd694c25b825a1dc4d6e3b58fbda6937bb9fd8
Moholand <moho.akbarzadeh@gmail.com>
2021-11-21 11:16:34 +0330

    first commit

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="sidebar-inner">
+  <div class="user-info">
+    <div class="user-image">
+      <img width="100" src="{{ url('https://www.pngitem.com/pimgs/m/391-3918613_personal-service-platform-person-icon-circle-png-transparent.png') }}" >
+    </div>
+    <div class="user-name">
+      <h3>محمد حسین اکبرزاده</h3>
+    </div>
+  </div>
+  <hr>
+  <div class="sidebar-menu">
+    <ul class="list-group">
+      <li class="list-group-item">
+        داشبورد
+      </li>
+      <li class="list-group-item">
+        درخواست جدید
+      </li>
+      <li class="list-group-item">
+        اعلان‌ها
+      </li>
+    </ul>
+  </div>
+</div>
---
+++ resources/views/vacation/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.base')
+
+@section('content')
+  <div class="content-inner">
+    داشبورد
+  </div>
+@endsection
