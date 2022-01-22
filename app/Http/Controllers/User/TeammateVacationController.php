740d3ba5cc14076bcd28b90b42f05d0d04b70b6a
Moholand <moho.akbarzadeh@gmail.com>
2022-01-22 16:21:05 +0330

    add filters to teammate vacation page

--- app/Http/Controllers/User/TeammateVacationController.php
+++ app/Http/Controllers/User/TeammateVacationController.php
@@ -2,28 +2,39 @@
 
 namespace App\Http\Controllers\User;
 
+use Carbon\Carbon;
 use App\Models\Vacation;
 use Illuminate\Http\Request;
+use Hekmatinasser\Verta\Verta;
 use App\Http\Controllers\Controller;
 
 class TeammateVacationController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $vacations = auth()->user()->department()
-            ->with(['users.vacations' => function($query) {
-                // This isn`t work because order of users count first -- Fix it! 
-                $query->orderBy('vacations.updated_at', 'DESC');
-            }])
-            ->get()
-            ->pluck('users')
-            ->collapse()
-            ->pluck('vacations')
-            ->collapse();
+        $perPage = $request->perPage ?? 20; // 20 is the default if there is no perPage in the request
 
-        return view('user.teammate-vacations', [
-            'vacations' => $vacations
-        ]);
+        $vacations = Vacation::with('user')->whereIn('user_id', $this->getTeammatesIds()) // Find a better way for doing this!
+            ->when($request->search && $request->search !== null, function($query) use($request) {
+                // Search in the username column - notice user comes with eager loading
+                $query->whereHas('user', function($nested_query) use($request) {
+                    $nested_query->where('name', 'LIKE', '%' . $request->search . '%');
+                });
+            })
+            ->when($request->vacation_status && $request->vacation_status !== null, function($query) use($request) {
+                $query->where('status', $request->vacation_status);
+            })
+            ->when($request->fromDate && $request->toDate, function($query) use($request) {
+                //convert dates to gerogrian -- return array of from and to dates
+                $dates = $this->dateToGerogrian($request->fromDate, $request->toDate);
+
+                $query->whereBetween('updated_at', [$dates['fromDate'], $dates['toDate']]);
+            })
+            ->orderBy('updated_at', 'DESC')
+            ->paginate($perPage)
+            ->withQueryString();;
+
+        return view('user.teammate_vacations', ['vacations' => $vacations]);
     }
 
     public function update(Request $request, Vacation $teammate_vacation)
@@ -39,5 +50,46 @@ class TeammateVacationController extends Controller
 
             return redirect()->back()->with('successMessage', 'تغییرات با موفقیت ذخیره شد');
         }
+    }
+
+    public function getTeammatesIds() 
+    {
+        $user_ids = [];
+
+        $users = auth()->user()->department()->with(['users'])
+        ->get()
+        ->pluck('users')
+        ->collapse();
+
+        foreach ($users as $user) {
+            array_push($user_ids, $user->id);
+        }
+
+        return $user_ids;
+    }
+
+    public function dateToGerogrian($fromDate, $toDate) : array
+    {
+        // Convert to gerogrian date manually -- any better idea??
+        $fromDateGerogrian = implode('-', Verta::getGregorian(
+            explode('/',$fromDate)[0],
+            explode('/',$fromDate)[1],
+            explode('/',$fromDate)[2]
+        ));
+        
+        $toDateGerogrian = implode('-', Verta::getGregorian(
+            explode('/',$toDate)[0],
+            explode('/',$toDate)[1],
+            explode('/',$toDate)[2]
+        ));
+
+        // Delete the effect of clock
+        $fromDateStandard = Carbon::createFromFormat('Y-m-d', $fromDateGerogrian)->startOfDay();
+        $toDateStandard = Carbon::createFromFormat('Y-m-d', $toDateGerogrian)->endOfDay();
+
+        return [
+            'fromDate' => $fromDateStandard,
+            'toDate' => $toDateStandard
+        ];
     }
 }
---
+++ resources/views/user/teammate_vacations.blade.php
@@ -0,0 +1,225 @@
+@extends('layouts.base')
+
+@push('stylesheets')
+  <link type="text/css" rel="stylesheet" href="{{ asset('css/jalalidatepicker.min.css') }}" />
+@endpush
+
+@section('filters')
+  {{-- PerPage options --}}
+  <li class="list-group-item bg-transparent d-flex align-items-center mr-1 mr-md-5 py-4">
+    <form id="perPage-form">
+      <div class="form-group my-0">
+
+        {{-- Hidden input fields for keeping other query strings -- any better idea?? --}}
+        @if(request()->fromDate || request()->toDate || request()->vacation_status)
+          <input type="hidden" name="fromDate" value="{{ request()->fromDate ?? null }}"/>
+          <input type="hidden" name="toDate" value="{{ request()->toDate ?? null }}"/>
+          <input type="hidden" name="vacation_status" value="{{ request()->vacation_status ?? null }}"/>
+        @endif
+
+        <select 
+          class="form-control-sm text-secondary border-0 perPage-select" 
+          id="perPageSelect" name="perPage" onchange="this.form.submit()"
+        >
+          <option selected="true" disabled="disabled">فیلتر تعداد</option>
+          @for ($count = 10; $count <= 50; $count += 10)
+            <option 
+              value="{{ $count }}" 
+              {{ request()->get('perPage') == $count ? 'selected' : '' }}
+            >
+              {{ $count }}
+            </option>
+          @endfor
+        </select>
+      </div>
+    </form>
+  </li>
+
+  {{-- Select Vacation status --}}
+  <li class="list-group-item bg-transparent d-flex align-items-center py-4">
+    <form>
+      <div class="form-group my-0">
+
+        {{-- Hidden input fields for keeping other query strings -- any better idea?? --}}
+        @if(request()->fromDate || request()->toDate || request()->perPage)
+          <input type="hidden" name="fromDate" value="{{ request()->fromDate ?? null }}"/>
+          <input type="hidden" name="toDate" value="{{ request()->toDate ?? null }}"/>
+          <input type="hidden" name="perPage" value="{{ request()->perPage ?? null }}"/>
+        @endif
+
+        <select class="form-control-sm text-secondary border-0" name="vacation_status" onchange="this.form.submit()">
+          <option value="">همه‌ی درخواست‌ها</option>
+          <option value="submitted" {{ request()->get('vacation_status') === 'submitted' ? 'selected' : '' }}>
+            ارسال شده
+          </option>
+          <option value="initial-approval" {{ request()->get('vacation_status') === 'initial-approval' ? 'selected' : '' }}>
+            تأیید اولیه
+          </option>
+          <option value="confirmed" {{ request()->get('vacation_status') === 'confirmed' ? 'selected' : '' }}>
+            تأیید نهایی
+          </option>
+          <option value="refuse" {{ request()->get('vacation_status') === 'refuse' ? 'selected' : '' }}>
+            عدم تأیید
+          </option>
+        </select>
+      </div>
+    </form>
+  </li>
+
+  {{-- Date Range options --}}
+  <li class="list-group-item bg-transparent d-flex align-items-center date-filter py-4">
+    <form autocomplete="off">
+      {{-- Hidden input fields for keeping other query strings -- any better idea?? --}}
+      @if(request()->perPage || request()->vacation_status)
+        <input type="hidden" name="perPage" value="{{ request()->perPage ?? null }}"/>
+        <input type="hidden" name="vacation_status" value="{{ request()->vacation_status ?? null }}"/>
+      @endif
+      <div class="form-row">
+        <div class="col-4">
+          <input 
+            type="text" 
+            name="fromDate"
+            class="form-control-sm border-0 from-date-input" 
+            placeholder="تاریخ شروع"
+            data-jdp
+            value="{{ request()->get('fromDate') ?? '' }}"
+            required
+          >
+        </div>
+        <div class="col-4">
+          <input 
+            type="text" 
+            name="toDate"
+            class="form-control-sm border-0 to-date-input" 
+            placeholder="تاریخ پایان"
+            data-jdp
+            value="{{ request()->get('toDate') ?? '' }}"
+            required
+          >
+        </div>
+        <div class="col-4">
+          <button 
+            type="submit" 
+            class="btn btn-secondary btn-sm"
+          >
+            اعمال فیلتر
+          </button>
+        </div>
+      </div>
+    </form>
+  </li>
+@endsection
+
+@section('content')
+  <h4>درخواست‌های سایر اعضای گروه</h4>
+  <hr class="mb-0 header-line">
+
+  @include('includes.successMessage')
+
+  <div class="vacations-list content-list">
+    <div id="accordion">
+      @forelse($vacations as $key => $vacation)
+
+        @php
+          // Calculate Local date
+          Verta::setStringformat('H:i Y-n-j');
+          $request_date = new Verta($vacation->updated_at);
+        @endphp
+        
+        <div class="card mb-4">
+          <div class="card-header py-2 px-4 d-flex justify-content-between align-items-center vacation-header" id="heading-{{ $key }}" data-toggle="collapse" data-target="#collapse-{{ $key }}" aria-expanded="false" aria-controls="collapse-{{ $key }}">
+            <div class="request-info d-flex justify-content-between align-items-center">
+              <h6 class="mb-0 vacation-title">
+                {{ ($key + 1) + (($vacations->currentPage() - 1) * $vacations->perPage()) }}) 
+                <span>نام:</span> {{ $vacation->user->name }} 
+                <span class="small" dir="rtl">({{ $request_date }})</span>
+              </h6>
+              <div class="status font-weight-bold">
+                <span>وضعیت:</span>
+                <span class="mr-2 text-white badge badge-{{ translate_status($vacation->status)['status_class'] }}">
+                  {{ translate_status($vacation->status)['status'] }}
+                </span>
+              </div>
+            </div>
+            <span class="btn btn-link">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+              </svg>
+            </span>
+          </div>
+      
+          <div id="collapse-{{ $key }}" class="collapse" aria-labelledby="heading-{{ $key }}" data-parent="#accordion">
+            <div class="card-body">
+              <span class="font-weight-bold">عنوان درخواست:</span>
+              <p>{{ $vacation->title }}</p>
+              <span class="font-weight-bold">متن درخواست:</span>
+              <p>{{ $vacation->request_message }}</p>
+              <div class="row mb-3">
+                <div class="col-md-4">
+                  <span class="font-weight-bold">نوع مرخصی:</span> <span>{{ translate_type($vacation->type) }}</span>
+                </div>
+                <div class="col-md-4">
+                  <span class="font-weight-bold">حالت مرخصی:</span> <span>{{ translate_mode($vacation->mode) }}</span>
+                </div>
+              </div>
+              <div class="row mr-0">
+                <div class="com-md-4">
+                  <span class="font-weight-bold">از تاریخ:</span> <span>&#x200E;{{ $vacation->from_date }}</span>
+                  @if($vacation->mode === 'daily')
+                    <span class="font-weight-bold mr-5">تا تاریخ:</span> <span>&#x200E;{{ $vacation->to_date }}</span>
+                  @endif
+                </div>
+                @if($vacation->mode === 'hourly')
+                <div class="col-md-4">
+                  <span class="font-weight-bold">از ساعت:</span> <span>{{ $vacation->from_hour }}</span>
+                  <span class="mr-5 font-weight-bold">تا ساعت:</span> <span>{{ $vacation->to_hour }}</span>
+                </div>
+                @endif
+              </div>
+              {{-- Only vacations with status of "submitted" can be updated --}}
+              @if($vacation->status === 'submitted')
+                <hr>
+                <form 
+                  action="{{ route('teammate-vacations.update', ['teammate_vacation' => $vacation->id]) }}" 
+                  method="POST"
+                >
+                  @csrf
+                  @method('PATCH')
+
+                  <div class="form-group mb-0 d-flex justify-content-end">
+                    <button type="submit" name="submit" value="confirm" class="btn btn-sm btn-success">
+                      تأیید
+                    </button>
+                    <button type="submit" name="submit" value="refuse" class="btn btn-sm btn-secondary mr-2">
+                      عدم تأیید
+                    </button>
+                  </div>
+
+                </form>
+              @endif
+            </div>
+          </div>
+        </div>
+
+      @empty
+        <div class="alert alert-danger p-4 text-center border-0 font-weight-bold">
+          متأسفانه درخواستی یافت نشد !!!
+        </div>
+      @endforelse
+
+      {{ $vacations->onEachSide(2)->links('vendor.pagination.default') }}
+
+    </div>
+  </div>
+
+@endsection
+
+@push('scripts')
+  <script type="text/javascript" src="{{ asset('js/jalalidatepicker.min.js') }}"></script>
+  <script>
+    $( document ).ready(function() {
+      // Date picker start
+      jalaliDatepicker.startWatch();
+    });
+  </script>
+@endpush
