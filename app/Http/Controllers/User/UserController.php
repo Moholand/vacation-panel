5ef1e8269e36d7e4b03f5b6cd901ab447ccce912
Moholand <moho.akbarzadeh@gmail.com>
2022-02-27 18:14:02 +0330

    refactoring

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\User;
 
 use App\Models\User;
 use App\Models\Department;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\UserUpdateRequest;
